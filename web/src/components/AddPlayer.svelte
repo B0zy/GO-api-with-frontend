854ec8a841f74b9f0f19b451c6f0d09c7512573b
B0zy <40073828+B0zy@users.noreply.github.com>
2024-04-19 12:43:08 -0400

    Keep hardcoded path for now or else it errors

--- web/src/components/AddPlayer.svelte
+++ web/src/components/AddPlayer.svelte
@@ -1,44 +1,43 @@
 <script lang="ts">
-    let formData = {
-      Username: '',
-      Password: '',
-      CharName: ''
-    };
-  
-    async function addPlayer() {
-      try {
-        const url = import.meta.env.API_URL;
-        const response = await fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(formData)
-        });
-  
-        if (!response.ok) {
-          throw new Error(`Failed to add player: ${response.status} ${response.statusText}`);
-        }
-  
-        console.log('Player added successfully!');
-      } catch (error) {
-        console.error('Error adding player:', error.message);
+  let formData = {
+    Username: '',
+    Password: '',
+    CharName: ''
+  };
+
+  async function addPlayer() {
+    try {
+      const url = import.meta.env.API_URL;
+      const response = await fetch("http://127.0.0.1:8090/players", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData) // Ensure formData is correctly populated
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to add player: ${response.status} ${response.statusText}`);
       }
+
+      console.log('Player added successfully!');
+    } catch (error) {
+      console.error('Error adding player:', error.message);
     }
+  }
 </script>
-  
-  <h2>Add New Player</h2>
-  
-  <form on:submit|preventDefault={addPlayer}>
-    <label for="username">Username:</label>
-    <input type="text" id="username" bind:value={formData.Username} required />
-  
-    <label for="password">Password:</label>
-    <input type="password" id="password" bind:value={formData.Password} required />
-  
-    <label for="charname">Character Name:</label>
-    <input type="text" id="charname" bind:value={formData.CharName} required />
-  
-    <button type="submit">Add Player</button>
-  </form>
-  
+
+<h2>Add New Player</h2>
+
+<form on:submit|preventDefault={addPlayer}>
+  <label for="username">Username:</label>
+  <input type="text" id="username" bind:value={formData.Username} required />
+
+  <label for="password">Password:</label>
+  <input type="password" id="password" bind:value={formData.Password} required />
+
+  <label for="charname">Character Name:</label>
+  <input type="text" id="charname" bind:value={formData.CharName} required />
+
+  <button type="submit">Add Player</button>
+</form>
