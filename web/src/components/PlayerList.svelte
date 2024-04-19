1bbae92054c30b3c049a5cd687747f028eb2bf32
B0zy <40073828+B0zy@users.noreply.github.com>
2024-04-19 14:13:39 -0400

    Formatting PlayerList.svelte

--- web/src/components/PlayerList.svelte
+++ web/src/components/PlayerList.svelte
@@ -6,29 +6,29 @@
   
     async function fetchPlayers() {
       try {
-        const response = await fetch('http://127.0.0.1:8090/players');
-        if (!response.ok) {
-          throw new Error('Failed to fetch player data');
-        }
-        const data = await response.json();
-        players.set(data.data);
-      } catch (error) {
-        console.error('Error fetching players:', error.message);
+      const response = await fetch('http://127.0.0.1:8090/players');
+      if (!response.ok) {
+        throw new Error('Failed to fetch player data');
       }
+      const data = await response.json();
+      players.set(data.data);
+    } catch (error) {
+      console.error('Error fetching players:', error.message);
     }
-  </script>
+  }
+</script>
   
-  <div>
-    <h2>Player List</h2>
-    <button on:click={fetchPlayers}>Fetch Players</button>
+<div>
+  <h2>Player List</h2>
+  <button on:click={fetchPlayers}>Fetch Players</button>
   
-    {#if $players.length > 0}
-      <ul>
-        {#each $players as player}
-          <li>Username: {player.Username} - CharName: {player.CharName}</li>
-        {/each}
-      </ul>
-    {:else}
-      <p>No players found</p>
-    {/if}
-  </div>
+  {#if $players.length > 0}
+    <ul>
+      {#each $players as player}
+        <li>Username: {player.Username} - CharName: {player.CharName}</li>
+      {/each}
+    </ul>
+  {:else}
+    <p>No players found</p>
+  {/if}
+</div>
